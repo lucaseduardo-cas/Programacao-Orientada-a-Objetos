237a1c6ade74c45e77f8b71c0f09d432f49d4e95
Lucas <lucas.alfra@gmail.com>
2025-08-19 12:58:13 -0300

    Reorganizando repositório

--- estudos/project/src/mensagem/MensagemTexto.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "MensagemTexto.hpp"
-
-MensagemTexto::MensagemTexto(std::string msg) {
-    this->_msg = msg;
-}
-void MensagemTexto::exibir() {
-    std::cout << this->_msg << std::endl;
-}
